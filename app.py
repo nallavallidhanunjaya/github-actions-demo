e21975056cc4b8fe4828b5d4ae55125c3bb95b79
Dhanunjaya Nallavalli <nallavallidhanunjaya02@gmail.com>
2026-03-15 18:37:14 +0530

    updated note

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to GitHub Actions CI/CD"
+    return "Welcome to GitHub Actions CI/CD - updated version!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
