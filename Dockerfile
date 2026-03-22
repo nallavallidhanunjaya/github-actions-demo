507945b3b12c9c68a861a5265391f2f693f13ae4
Dhanunjaya Nallavalli <nallavallidhanunjaya02@gmail.com>
2026-03-22 21:22:22 +0530

    updated for monitoring

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,11 @@ WORKDIR /app
 
 COPY requirements.txt .
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
 EXPOSE 5000
+EXPOSE 8000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 import socket
 from datetime import datetime
+from prometheus_client import start_http_server, Counter
 
 app = Flask(__name__)
 
+# Prometheus metric
+REQUEST_COUNT = Counter('request_count', 'Total number of requests')
+
 @app.route("/")
 def home():
+    REQUEST_COUNT.inc()
     hostname = socket.gethostname()
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return render_template("index.html", hostname=hostname, time=time)
 
 if __name__ == "__main__":
+    # Start Prometheus metrics server
+    start_http_server(8000)
+
     app.run(host="0.0.0.0", port=5000)
